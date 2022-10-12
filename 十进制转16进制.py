13d8a773aa3820e78404b042565c1e21669ec4cd
imperfectlalala <91323648+imperfectlalala@users.noreply.github.com>
2022-10-12 12:29:42 +0800

    Update 十进制转16进制.py

--- 十进制转16进制.py
+++ 十进制转16进制.py
@@ -4,5 +4,6 @@ os.system("cls")
 decimal_num = int(input("请输入十进制数N："))
 process_num = int(bin(decimal_num),2)
 temp_num = hex(process_num)
+#这行代码的作用用来去掉显示结果的时候前面出现的0x，方便人类用户读取
 output_display = temp_num[2:]
 print("对应的十六进制数是：{}".format(output_display))
